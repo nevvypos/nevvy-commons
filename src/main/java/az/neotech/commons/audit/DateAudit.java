3acf545d366759da1b868c583448d161285814c0
mammadyahyayev <mammadyahyayev@gmail.com>
2025-10-28 21:01:04 +0400

    add entity listeners and spring boot audit annotations for audit classes

--- src/main/java/az/neotech/commons/audit/DateAudit.java
+++ src/main/java/az/neotech/commons/audit/DateAudit.java
@@ -1,16 +1,24 @@
 package az.neotech.commons.audit;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Setter
 @Getter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class DateAudit {
 
+    @CreatedDate
     protected LocalDateTime createdAt;
+
+    @LastModifiedDate
     protected LocalDateTime modifiedAt;
 }
--- src/main/java/az/neotech/commons/audit/DetailedAudit.java
+++ src/main/java/az/neotech/commons/audit/DetailedAudit.java
@@ -1,17 +1,31 @@
 package az.neotech.commons.audit;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class DetailedAudit {
+    @CreatedBy
     protected String createdBy;
+
+    @LastModifiedBy
     protected String modifiedBy;
+
+    @CreatedDate
     protected LocalDateTime createdAt;
+
+    @LastModifiedDate
     protected LocalDateTime modifiedAt;
 }
