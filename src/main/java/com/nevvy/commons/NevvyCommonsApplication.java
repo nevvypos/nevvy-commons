4979c717868ee223285b0519b9567d18c8ed1b5c
mammadyahyayev <mammadyahyayev@gmail.com>
2025-11-14 15:32:21 +0400

    feat: add missing main class

---
+++ src/main/java/com/nevvy/commons/NevvyCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.nevvy.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NevvyCommonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NevvyCommonsApplication.class, args);
+    }
+
+}
